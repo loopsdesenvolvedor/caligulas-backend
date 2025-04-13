9422c680fdd35090278db35c2a9aa377eaae57ff
Elizeu <loopsdesenvolvedor@gmail.com>
2025-04-13 02:15:51 -0300

    feat: add routes

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express, { NextFunction, Request, Response } from "express";
 import dotenv from "dotenv";
+import { router } from "./routes";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+app.use(router);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof Error) {
