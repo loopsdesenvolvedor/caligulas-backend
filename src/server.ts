6fb6d4a78c72d41cb488f55983706bb3b16e1ef6
Elizeu <loopsdesenvolvedor@gmail.com>
2025-04-13 02:08:30 -0300

    feat:  add inital config

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import express, { NextFunction, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    res.status(400).json({
+      err: err.message,
+    });
+
+    return;
+  }
+
+  res.status(500).json({
+    status: "Error",
+    message: "Internal server error",
+  });
+
+  return;
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`App is running on port: ${process.env.PORT} `);
+});
